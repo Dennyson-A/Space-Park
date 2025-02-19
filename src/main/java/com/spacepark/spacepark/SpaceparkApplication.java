635f891d8f2442a84d210015d45fdc3e6d177a4b
PRIYA <priya.26csb@licet.ac.in>
2025-02-19 11:56:29 +0530

    First commit

---
+++ src/main/java/com/spacepark/spacepark/SpaceparkApplication.java
@@ -0,0 +1,13 @@
+package com.spacepark.spacepark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpaceparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpaceparkApplication.class, args);
+	}
+
+}
